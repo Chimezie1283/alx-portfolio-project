f756ba27b25f5869573e0c4d4949dd8a2fd4bbc5
chimezie1283 <agbo.chimezie1283@gmail.com>
2024-03-14 14:16:01 +0100

    all

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb)  {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.k4dz88e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
     await client.connect();
 
     db = client.db('react-blog-db');
